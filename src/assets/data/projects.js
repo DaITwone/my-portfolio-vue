29cb3a8cc5a2788ff1451bbb737e2d346d97e097
_Dal___ <daittwone@gmail.com>
2025-07-04 11:36:05 +0700

    Update project files

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -4,7 +4,7 @@ export const projects = [
     description: 'A responsive personal website showcasing my projects',
     image: 'myportfolio.png',
     tech: ['HTML', 'CSS', 'JavaScript','Vue.js'],
-    github: 'https://github.com/yourusername/portfolio',
+    github: 'https://github.com/DaITwone/my-portfolio-vue',
     live: 'https://my-portfolio-web-847eb.web.app/'
   },
   {
@@ -12,7 +12,7 @@ export const projects = [
     description: 'Faculty management system built with Vue & Supabase',
     image: 'itdepartment.png',
     tech: ['HTML', 'CSS', 'JavaScript', 'Vue.js'],
-    github: 'https://github.com/yourusername/it-department',
+    github: 'https://github.com/DaITwone/it-department-vue',
     live: 'https://it-department-5f21c.web.app/'
   }
 ]
